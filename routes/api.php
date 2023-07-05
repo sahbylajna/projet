c885f3bb444712b56af496f2ead64bb3b84fa81b
sahbylajna <sahby.lajna@gmail.com>
2023-07-05 14:28:14 +0100

    contries

--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/login', [ClientsController::class, 'create_token']);
 
 Route::get('contries', function () {
-    $contries = App\Models\countries::where('active','1')->pluck('name','id')->all();
+    $contries = App\Models\countries::where('active','1')->get();
     return response()->json([
         'contries' => $contries
 
