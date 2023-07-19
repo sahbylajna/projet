729b435b3ca1db62ef685707e47250d525415f72
sahbylajna <sahby.lajna@gmail.com>
2023-07-19 13:37:22 +0100

    Update Navbar.blade.php

--- resources/views/layouts/Navbar.blade.php
+++ resources/views/layouts/Navbar.blade.php
@@ -1,13 +1,13 @@
  <!-- Navbar -->
  <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur" navbar-scroll="true">
       <div class="container-fluid py-1 px-3">
-        <nav aria-label="breadcrumb">
+        {{-- <nav aria-label="breadcrumb">
           <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 ">
             <li class="breadcrumb-item text-sm ps-2"><a class="opacity-5 text-dark" href="javascript:;">لوحات القيادة</a></li>
             <li class="breadcrumb-item text-sm text-dark active" aria-current="page">RTL</li>
           </ol>
           <h6 class="font-weight-bolder mb-0">RTL</h6>
-        </nav>
+        </nav> --}}
         <div class="collapse navbar-collapse mt-sm-0 mt-2 px-0" id="navbar">
 
           <ul class="navbar-nav me-auto ms-0 justify-content-end">
