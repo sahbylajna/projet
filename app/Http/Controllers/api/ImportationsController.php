f8cf6455816a513e616b0b34f2d4923e05807456
sahbylajna <sahby.lajna@gmail.com>
2023-08-22 16:55:24 +0100

    nullable

--- app/Http/Controllers/api/ImportationsController.php
+++ app/Http/Controllers/api/ImportationsController.php
@@ -224,7 +224,7 @@ class ImportationsController extends Controller
 
             'EXP_NATIONALITY' => 'string|min:1|nullable',
             'EXP_PASSPORT_NUM' => 'string|min:1|nullable',
-            'ANIMAL_INFO' => 'required'
+            'ANIMAL_INFO' => 'nullable'
 
         ];
         $validator = \Validator::make($request->all(),  $rules);
