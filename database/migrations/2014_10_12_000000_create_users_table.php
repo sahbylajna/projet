0cfd307c46c115d0c074141e529615aa0a324910
sahbylajna <sahby.lajna@gmail.com>
2023-09-22 11:06:19 +0100

    deleted

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->string('ud')->unique();
-
+            $table->softDeletes();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
