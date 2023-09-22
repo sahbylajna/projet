a1f316f9e856a11707b654c8ebde3d7c0e7a2b83
sahbylajna <sahby.lajna@gmail.com>
2023-09-22 12:51:04 +0100

    d

--- resources/views/importations/show.blade.php
+++ resources/views/importations/show.blade.php
@@ -6,7 +6,7 @@
     <div class="panel-heading clearfix">
 
         <span class="pull-left">
-            <h4 class="mt-5 mb-5">{{ isset($title) ? $title : 'Importation' }}</h4>
+            <h4 class="mt-5 mb-5">{{ isset($title) ? $title : 'Importation' }} <dd>{{ $importation->CER_SERIAL }}</dd></h4>
         </span>
 
         <div class="pull-right">
