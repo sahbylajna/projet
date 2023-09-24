2e9ac0d9fa40a2dc5fc5761f4efb1a183f1120d5
sahbylajna <sahby.lajna@gmail.com>
2023-09-24 21:02:08 +0100

    Update index.blade.php

--- resources/views/exports/index.blade.php
+++ resources/views/exports/index.blade.php
@@ -38,26 +38,18 @@
                         <tr>
                             <th>{{ trans('exports.client_id') }}</th>
                             <th>{{ trans('exports.CER_TYPE') }}</th>
-                            <th>{{ trans('exports.CER_LANG') }}</th>
+
                             <th>{{ trans('exports.COMP_ID') }}</th>
                             <th>{{ trans('exports.EUSER_QID') }}</th>
                             <th>{{ trans('exports.EXP_NAME') }}</th>
                             <th>{{ trans('exports.EXP_TEL') }}</th>
                             <th>{{ trans('exports.EXP_FAX') }}</th>
-                            <th>{{ trans('exports.EXP_COUNTRY') }}</th>
+
                             <th>{{ trans('exports.IMP_NAME') }}</th>
                             <th>{{ trans('exports.IMP_QID') }}</th>
                             <th>{{ trans('exports.IMP_FAX') }}</th>
                             <th>{{ trans('exports.IMP_TEL') }}</th>
-                            <th>{{ trans('exports.IMP_COUNTRY') }}</th>
-                            <th>{{ trans('exports.ORIGIN_COUNTRY') }}</th>
-                            <th>{{ trans('exports.SHIPPING_PLACE') }}</th>
-                            <th>{{ trans('exports.TRANSPORT') }}</th>
-                            <th>{{ trans('exports.SHIPPING_DATE') }}</th>
-                            <th>{{ trans('exports.APPLICANT_NAME') }}</th>
-                            <th>{{ trans('exports.APPLICANT_TEL') }}</th>
-                            <th>{{ trans('exports.EXP_NATIONALITY') }}</th>
-                            <th>{{ trans('exports.EXP_PASSPORT_NUM') }}</th>
+
                             <th>{{ trans('exports.accepted') }}</th>
                             <th>{{ trans('exports.reson') }}</th>
 
@@ -69,26 +61,18 @@
                         <tr>
                             <td>{{ optional($export->client)->ud }}</td>
                             <td>{{ $export->CER_TYPE }}</td>
-                            <td>{{ $export->CER_LANG }}</td>
+
                             <td>{{ $export->COMP_ID }}</td>
                             <td>{{ $export->EUSER_QID }}</td>
                             <td>{{ $export->EXP_NAME }}</td>
                             <td>{{ $export->EXP_TEL }}</td>
                             <td>{{ $export->EXP_FAX }}</td>
-                            <td>{{ $export->EXP_COUNTRY }}</td>
+
                             <td>{{ $export->IMP_NAME }}</td>
                             <td>{{ $export->IMP_QID }}</td>
                             <td>{{ $export->IMP_FAX }}</td>
                             <td>{{ $export->IMP_TEL }}</td>
-                            <td>{{ $export->IMP_COUNTRY }}</td>
-                            <td>{{ $export->ORIGIN_COUNTRY }}</td>
-                            <td>{{ $export->SHIPPING_PLACE }}</td>
-                            <td>{{ $export->TRANSPORT }}</td>
-                            <td>{{ $export->SHIPPING_DATE }}</td>
-                            <td>{{ $export->APPLICANT_NAME }}</td>
-                            <td>{{ $export->APPLICANT_TEL }}</td>
-                            <td>{{ $export->EXP_NATIONALITY }}</td>
-                            <td>{{ $export->EXP_PASSPORT_NUM }}</td>
+
                             <td>{{ $export->accepted }}</td>
                             <td>{{ $export->reson }}</td>
 
