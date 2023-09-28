ab20d946654e807d2e79a04e898550c2cf95b97c
sahbylajna <sahby.lajna@gmail.com>
2023-09-28 15:42:31 +0100

    Update ClientsController.php

--- app/Http/Controllers/api/ClientsController.php
+++ app/Http/Controllers/api/ClientsController.php
@@ -375,12 +375,7 @@ $data = json_encode($data);
             'headers' => $headers,
             'form_params' => $data,
         ]);
-        return response()->json([
 
-            $res
-
-
-        ]);
         $responseBody = $res->getBody()->getContents();
         $resp = json_decode($responseBody);
         return response()->json([
